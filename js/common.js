2cae8473b26c7bb5dffcebebd1c10659b5c4ce9b
MaxSakur <sakurwork@gmail.com>
2017-10-06 10:04:10 +0300

    ToTopButton + Hover-ы + Некоторые доработки

--- js/common.js
+++ js/common.js
@@ -1,5 +1,18 @@
 $(function() {
 
-	// Custom JS
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
+		} else {
+			$('.scrollup').fadeOut();
+		}
+	});
+
+	$('.scrollup').click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 600);
+		return false;
+	});
 
 });
